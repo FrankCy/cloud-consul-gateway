52f87bc023d25996ccba2d2ab960943a7fcf9ceb
cyoung <cy880708@163.com>
2019-01-18 15:37:20 +0800

    新增Consul-config代码，演示配置yml或properties

---
+++ cloud-csl-config-customize/src/main/java/com/sc/csl/config/ConsulConfigCustomizeApplication.java
@@ -0,0 +1,21 @@
+package com.sc.csl.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: cloud-master
+ * @package: com.sc.csl.config、
+ * @email: cy880708@163.com
+ * @date: 2019/1/18 下午2:22
+ * @mofified By:
+ */
+@SpringBootApplication
+public class ConsulConfigCustomizeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulConfigCustomizeApplication.class, args);
+    }
+}
---
+++ cloud-csl-config-customize/src/main/java/com/sc/csl/config/controller/ConsulConfigCustomizeController.java
@@ -0,0 +1,43 @@
+package com.sc.csl.config.controller;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: cloud-master
+ * @package: com.sc.csl.config.controller、
+ * @email: cy880708@163.com
+ * @date: 2019/1/18 下午2:23
+ * @mofified By:
+ */
+@RestController
+public class ConsulConfigCustomizeController {
+
+    /**
+     * 日志
+     */
+    private static final Log logger = LogFactory.getLog(ConsulConfigCustomizeController.class);
+
+    /**
+     * 读取远程配置
+     */
+    @Value("${foo.bar.name}")
+    private String name;
+
+    /**
+     * 将配置展示在页面
+     * @return
+     */
+    @GetMapping("/getName")
+    public String getName() {
+        logger.info("配置是：" + name);
+        return name;
+    }
+
+}
