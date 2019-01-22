c69205c999c80382fe5f06692768e9e63b2fb455
cyoung <cy880708@163.com>
2019-01-22 13:26:01 +0800

    实现简单网关路由

---
+++ cloud-gty-basic-application/src/main/java/com/sc/gty/basic/CloudGatewayBasic1Application.java
@@ -0,0 +1,37 @@
+package com.sc.gty.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: cloud-master
+ * @package: PACKAGE_NAME、
+ * @email: cy880708@163.com
+ * @date: 2019/1/21 下午6:42
+ * @mofified By:
+ */
+@SpringBootApplication
+public class CloudGatewayBasic1Application {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                // basic proxy
+                .route(r -> r.path("/jd")
+                                .uri("http://jd.com:80/")
+                                    .id("jd_route")
+                        ).build();
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayBasic1Application.class, args);
+    }
+
+}
---
+++ cloud-gty-basic-yml/src/main/java/com/sc/gty/basic/CloudGatewayBasic2Application.java
@@ -0,0 +1,23 @@
+package com.sc.gty.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @version 1.0
+ * @description：
+ * @author: Yang.Chang
+ * @project: cloud-master
+ * @package: PACKAGE_NAME、
+ * @email: cy880708@163.com
+ * @date: 2019/1/21 下午6:42
+ * @mofified By:
+ */
+@SpringBootApplication
+public class CloudGatewayBasic2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayBasic2Application.class, args);
+    }
+
+}
